893998b098b224d8736f874f10c2934543c90eb0
Marvin Rubio <marvinjay.rubio@ssu.edu.ph>
2024-12-22 21:46:03 +0800

    fix nav design

--- view/partials/nav.php
+++ view/partials/nav.php
@@ -1,11 +1,11 @@
 
 <div class="container">
     <div class="row">
-        <nav class="navbar rounded-1 bg-dark navbar-expand-lg ">
+        <nav class="navbar navbar-dark bg-dark rounded-1 navbar-expand-lg ">
             <div class="container-fluid">
                 <a class="navbar-brand fw-bold text-white" href="#">Project Ideas</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler shadow-none mb-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon burger-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="d-flex justify-content-end navbar-nav me-auto mb-2 mb-lg-0 w-100">
