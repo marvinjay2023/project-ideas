f14b353c8dd192e642c05f232033b311b14007cc
Marvin Rubio <marvinjay.rubio@ssu.edu.ph>
2024-12-28 10:13:23 +0800

    temporary commented header and footer CDNs

--- view/partials/header.php
+++ view/partials/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="/bootstrap/css/datables.min.css">
 
     <!-- bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <!-- <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"> -->
 
     
