24a8cdaf472dce5fda62c782b1398315a3fdafc2
Marvin Rubio <marvinjay.rubio@ssu.edu.ph>
2024-12-28 10:05:19 +0800

    trying cdn for design for the table

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,7 +1,15 @@
-<script src="/bootstrap/js/bootstrap.min.js"></script>
+<!-- JQUERY LOCAL FILE -->
+<!-- <script src="/bootstrap/js/bootstrap.min.js"></script>
 <script src="/bootstrap/js/jquery.min.js"></script>
 <script src="/bootstrap/js/jquery.dataTables.min.js"></script>
-<script src="/bootstrap/js/dataTables.bootstrap.min.js"></script>
+<script src="/bootstrap/js/dataTables.bootstrap.min.js"></script> -->
+
+
+<!-- JQUERY CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 
 <script>
 
--- view/partials/header.php
+++ view/partials/header.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- Bootstrap -->
+
+    <!-- Bootstrap for local file -->
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap/css/datables.min.css">
+
+    <!-- bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"> -->
+
+    
 
     <!-- Custome Css -->
     <link rel="stylesheet" href="/bootstrap/css/custom.css">
